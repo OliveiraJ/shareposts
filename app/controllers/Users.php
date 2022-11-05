0fa84da02e3df0ff7d2b426d2190cae8f6b764d5
Jordan Oliveira <jordansilva102@gmail.com>
2022-11-05 18:45:49 -0300

    Application finished

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -130,7 +130,7 @@ class Users extends Controller {
         $_SESSION['user_email'] = $user->email;
         $_SESSION['user_name'] = $user->name;
 
-        redirect('pages/index');
+        redirect('posts');
     }
 
     public function logout() {
@@ -140,13 +140,5 @@ class Users extends Controller {
         session_destroy();
 
         redirect('users/login');
-    }
-
-    public function isLoggedIn() {
-        if (isset($_SESSION['user_id'])) {
-            return true;
-        } else {
-            return false;
-        }
     }
 }
